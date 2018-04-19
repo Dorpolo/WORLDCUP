4d96c4123102e35ed5767be08bc2d11c5707333f
Dor Polovincik <33598629+Dorpolo@users.noreply.github.com>
2018-04-19 23:20:05 +0300

    Update server.R

--- server.R
+++ server.R
@@ -1,9 +1,8 @@
+
 server <- function(input, output) {
   
   polo <- reactive({
-    user_results_validation %>% filter(Round %in% input$Round & 
-                                         Active_Included %in% input$inActive &
-                                         Group %in% input$Group & 
+    user_results_validation %>% filter(  Active_Included %in% input$inActive &
                                          GameID >= input$game_number[1] & 
                                          GameID <= input$game_number[2] ) })
   
@@ -24,8 +23,7 @@ server <- function(input, output) {
                   class = 'cell-border stripe') %>%  
       formatStyle(c('Rank','User Name','Points','Boom','Winning Goals','Pct','Games'),
                   color = '#586261')
-  })
-  
+  })  
   
   polo_2 <- reactive({
     user_results_validation %>% 
@@ -43,40 +41,27 @@ server <- function(input, output) {
             legend.direction = "horizontal",
             legend.title = element_blank())})
   
-  polo_3 <- reactive({ user_results_validation %>%
-      filter(`User Name` %in% input$userID) %>%
-      select(Date,User = `User Name`,Game = GameID ,user_game_points) %>%
-      group_by(Date,User) %>% 
-      summarise(Points = sum(user_game_points)) %>% 
-      group_by(User) %>% 
-      mutate(Cum = cumsum(Points)) %>% 
-      arrange(desc(Cum)) %>%
-      group_by(Date) %>% 
-      mutate(ranking = row_number())%>% 
-      arrange(Date) %>%
-      left_join(date.id,by = c("Date")) %>% mutate(alpha = 1, x = 0.5 ,x.2 = 16)
-  })
-  
-  output$lineplot <- renderPlot({
-    
-    ggplot(data = polo_3(), aes(x = Day, y = ranking, group = User)) +
+polo_3 <- reactive({user_rank_by_day %>% filter( User  %in% input$userID)})
+
+  output$lineplot<- renderPlot({
+        ggplot(data = polo_3(), aes(x = Day, y = Rank, group = User)) +
       geom_line(aes(color =  User,alpha = alpha), size = 2) +
       geom_point(aes(color = User,alpha = alpha), size = 4) +
       geom_point(color = "#FFFFFF", size = 1) +
-      scale_y_reverse(breaks = 1:nrow(polo_3())) +
-      scale_x_continuous(breaks = 1:16, expand = c(.05, .05)) +
+      scale_y_reverse(breaks=1:N_users) + 
+      scale_x_discrete(breaks = 1:current_competition_day, expand = c(.05, .05)) + 
       geom_text(data =  polo_3() %>% filter(Day == "1"),
-                aes(label = User, x = x) , hjust = .85, fontface = "bold", color = "#00384A", size = 4) +
-      geom_text(data = polo_3() %>% filter(Day == "15"),
-                aes(label = User, x = x.2) , hjust = 0.15, fontface = "bold", color = "#00384A", size = 4) +
-      coord_cartesian(ylim = c(1,length(unique(polo_3()$User)))) + 
+                aes(label = User, x = x) , hjust = .85, color = "#00384A", size = 3) +
+      geom_text(data = polo_3() %>% filter(Day == current_competition_day),
+                aes(label = User, x = x.2) , hjust = 0.15, color = "#00384A", size = 3) +
+      coord_cartesian(ylim = c(1,30)) + 
       theme(legend.position = "none") +
       labs(x = "Competition Days with games",
            y = "Rank",
            title = "World Cup as a Service",
-           subtitle = "Users ranked by overall points after each competition Day") + my_theme()
-  })
-  
+           subtitle = "Users ranked by overall points after each competition Day") + my_theme() })
+
+
   polo_4 <- reactive({ team_table_by_users  %>%
       filter(User %in% input$userID) 
   })
