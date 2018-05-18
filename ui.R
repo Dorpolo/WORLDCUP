228c91046674a5945c347b30c7cf2023ece735c5
Dor Polovincik <33598629+Dorpolo@users.noreply.github.com>
2018-05-18 14:52:37 +0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@
 
 
 ui <- fluidPage(theme = shinytheme("superhero"),
-
+                
                 # App title ----
                 titlePanel("World Cup Friends Zone"),
                 
@@ -13,18 +13,18 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                   # Sidebar panel for inputs ----
                   sidebarPanel(
                     
-
-                     HTML('<img src="https://fsprdcdnpublic.azureedge.net/global-pictures/tournaments-sq-4/254645_w" height="95" width="130" align="middle">'),
+                    
+                    HTML('<img src="https://fsprdcdnpublic.azureedge.net/global-pictures/tournaments-sq-4/254645_w" height="95" width="130" align="middle">'),
                     HTML('<img src="http://i65.tinypic.com/11ht1et.png" height="95" width="170" align="middle">'),
-            
-
-
-                      titlePanel(paste("League Filters : ")),
-             
-                      sliderInput(inputId ="game_number",
-                                  label ="Game Number:",
-                                  min = 1, max = 64,
-                                  value = c(1,N_games)),
+                    
+                    
+                    
+                    titlePanel(paste("League Filters : ")),
+                    
+                    sliderInput(inputId ="game_number",
+                                label ="Game Number:",
+                                min = 1, max = 64,
+                                value = c(1,N_games)),
                     
                     selectInput(inputId = "nameID",
                                 label =  "Game :",
@@ -36,19 +36,19 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                                 choices  = choices$userID,
                                 selected = choices$userID,
                                 multiple = TRUE),
-                  
-                      # Select variables for the PIE CHART
                     
-             
-              
-                      div(id = 'large',
-                      checkboxInput(inputId = "somevalue",
-                                    label = h4("Active Games Included"),
-                                    value = FALSE,
-                                    width = validateCssUnit(200)))
-                
+                    # Select variables for the PIE CHART
                     
-                    ),
+                    
+                    
+                    div(id = 'large',
+                        checkboxInput(inputId = "somevalue",
+                                      label = h4("Active Games Included"),
+                                      value = FALSE,
+                                      width = validateCssUnit(200)))
+                    
+                    
+                  ),
                   
                   # Main panel for displaying outputs ----
                   mainPanel(
@@ -68,11 +68,11 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                                 tabPanel(title =  'CUP',
                                          h4("Welcome to the CUP Zone"),
                                          fluidRow(column(8,
-                                        DT::dataTableOutput(outputId = "cup_table")), 
-                                         column(4,
-                                                uiOutput(outputId = "imagecup")))
+                                                         DT::dataTableOutput(outputId = "cup_table")), 
+                                                  column(4,
+                                                         uiOutput(outputId = "imagecup")))
                                          
-                                         ),
+                                ),
                                 tabPanel(title = "User Guesses",
                                          h4("All User Guesses"),
                                          DT::dataTableOutput(outputId = "user_guess"),
