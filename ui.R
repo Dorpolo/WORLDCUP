d666411da89e704784f6ca511733460968565608
Dor Polovincik <33598629+Dorpolo@users.noreply.github.com>
2018-06-19 18:28:40 +0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,4 +1,3 @@
-
 # This is the user-interface definition of a Shiny web application. You can
 # run the application by clicking 'Run App' above.
 #
@@ -6,7 +5,6 @@
 # 
 #    http://shiny.rstudio.com/
 #
-
 
 
 # WorldCup Friends Zone - Shiny App #####
@@ -23,6 +21,10 @@
   library(DT)
   library(scales)
   library(visNetwork)
+  library(lubridate)
+  library(XML)
+  library(RCurl)
+  library(httr)
 }
 
 # Loading Data Sets from Google Sheets
@@ -39,6 +41,57 @@
                                   "-1vQ4jRITA24Oj_h-i4cVxEGstFTS7-qKH0bv_pp61h-Jj4G",
                                   "0t-fLh6TUiZU-Qor1WA2pt50TJkENnCkh/pub?gid=0&single=true&output=csv")),
                        stringsAsFactors = FALSE)
+  
+  
+  
+  ### Fixtures & Score API Joined 
+  {
+    fixtures <- read.csv(url(paste0("https://docs.google.com/spreadsheets/d/e/2PACX-1vTNgD6oZivKRepzwPWDc",
+                                    "YMg4tOQQq8B3sJLFdtHHE7p8lYs4lv_C4Wk_B3lkAPx-nZA",
+                                    "4O6DETuEAhxw/pub?gid=0&single=true&output=csv")),
+                         stringsAsFactors = FALSE)
+    
+    partial_fixtures <- fixtures %>% select(-c(started,active,Active_Included,true_Home_Goals,true_Away_Goals,true_Direction))
+    
+    partial_fixtures$new_id <- as.character(partial_fixtures$new_id)
+    
+    url <- 'https://perodriguezl-russia-2018-worldcup-tournament-v1.p.mashape.com/public/tournament/1'
+    r <-  GET(url,add_headers(`X-Mashape-Key` = "GfEVgWsBovmshScrSarHgMWOihvjp17eUsvjsnwtfD1M1esYX9"))
+    fixt <-  as.data.frame(t(as.data.frame(unlist(content(r)$data[[1]]))),
+                           row.names = NULL)
+    
+    for( i in 2:64)
+    {
+      b <- as.data.frame(t(as.data.frame(unlist(content(r)$data[[i]]))),
+                         row.names = NULL)
+      fixt <- bind_rows(fixt,b)
+    }
+    
+    fixt$countdown <- as.numeric(fixt$countdown)
+    fixtures$new_id <- as.character(fixtures$new_id)
+    fixt_to_be_added <- fixt %>% select(GameID = id,
+                                        true_Home_Goals = results.home ,
+                                        true_Away_Goals = results.visitor ,
+                                        finished,
+                                        countdown) %>% mutate(started = ifelse(countdown == 0,TRUE,FALSE),
+                                                              active  = ifelse(finished  == FALSE & countdown == 0,TRUE,FALSE)) %>% 
+      left_join(fixtures %>% select(new_id,NameID),by = c('GameID'='new_id'))   %>% mutate(Active_Included = ifelse(started == TRUE & active == FALSE,'Complited Games',
+                                                                                                                    ifelse(started == TRUE & active == TRUE,'Active Games',
+                                                                                                                           'Future Games'))) %>% 
+      mutate(true_Direction  = ifelse(true_Home_Goals>true_Away_Goals,"Home",
+                                      ifelse(true_Home_Goals<true_Away_Goals,"Away",
+                                             "Draw"))) %>%
+      select(GameID,NameID,countdown,finished,started,active,Active_Included,true_Home_Goals,true_Away_Goals,true_Direction)
+    
+    partial_fixtures$new_id <- as.character(partial_fixtures$new_id)
+    
+    final <- partial_fixtures %>% inner_join(fixt_to_be_added,by = c('new_id'='GameID'))
+    
+    fixtures <- final
+
+  }
+  
+  
   
   # Users' score predictions - Group Stage
   
@@ -64,7 +117,7 @@
   
   knokout_col_adj <- fixtures %>% filter(Stage != "Group Stage") %>% 
     select(GameID,
-           original_fixture_name = NameID)  %>% 
+           original_fixture_name = NameID.y)  %>% 
     mutate(resultes_knokout_name = (names(resultes_knokout)[-c(1,2)])[seq(1,32,by=2)])
   
   
@@ -83,16 +136,6 @@
   # and we are all set for data manipulation
 }
 
-# all UI filter selection options
-{
-  choices <- list(
-    nameID = unique(fixtures$NameID),
-    userID = c(levels(sort(User_ID$`User_Nick`))),
-    Active =  unique(sort(fixtures$Active_Included)),
-    Cup = unique((fixtures %>% filter(Cup_Stage != "None"))$Cup_Stage)
-  )
-}
-
 # Extracting competition parameters 
 {
   N_users <- nrow(User_ID)
@@ -103,10 +146,19 @@
                          max(fixtures$GameID[which(fixtures$started==TRUE)])+1,
                          fixtures$GameID[which(fixtures$active == TRUE)[1]])
   
-  current_Game_Name <- (fixtures %>% filter(GameID == current_game) %>% select(NameID))[1,1]
+  current_Game_Name <- (fixtures %>% filter(GameID == current_game) %>% select(NameID.y))[1,1]
   
   current_cup_rank <-   fixtures$Cup_Rank[which(fixtures$GameID == current_game)]
   
+}
+
+# all UI filter selection options
+{
+  choices <- list(
+    nameID = unique(fixtures$NameID.y)[c(current_game:64,1:(current_game-1))],
+    userID = c(levels(sort(User_ID$`User_Nick`))),
+    Cup = unique((fixtures %>% filter(Cup_Stage != "None"))$Cup_Stage)
+  )
 }
 
 
@@ -159,6 +211,7 @@ ui <- fluidPage(theme = shinytheme("slate"),
                                          visNetworkOutput(outputId = "network",height = "500px")),
                                 tabPanel(title =  'CUP',
                                          h3("The FZ Cup"),
+                                         uiOutput(outputId = "pre_cup"),
                                          DT::dataTableOutput(outputId = "cup_table")       
                                 ),
                                 
